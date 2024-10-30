c440cd59a0624adc1e475af01bdacf976b800a52
Akanchha Kumari <akanchha.k29@gmail.com>
2024-10-30 17:10:23 +0530

    Updated header, navbar, styles, and script

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -1,50 +1,24 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="/js/scripts.js"></script>
-    <title>Arya Prawah</title>
-</head>
-<body>
-    <header class="container-fluid bg-light py-3">
-        <div class="row align-items-center">
-            <div class="col-12 col-md-4 d-flex align-items-center justify-content-center justify-content-md-start">
-                <div id="datetime" class="mr-3"></div>
-                <div id="city" class="mr-3">Lucknow</div>
-                <i id="weather-icon" class="fas fa-sun mr-2"></i>
-                <div id="temperature" class="mr-3">28°C</div>
-                <a href="/" class="mr-3"><i class="fas fa-home"></i></a>
-            </div>
-            <div class="col-12 col-md-4 text-center">
-                <h1>Arya Prawah</h1>
-            </div>
-            <div class="col-12 col-md-4 text-center text-md-right">
-                <a href="/login"><i class="fas fa-user-circle"></i>Login</a>
-            </div>
-        </div>
-    </header>
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Arya Prawah</title>
+
     <link rel="stylesheet" href="/css/style.css">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search" />
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <script src="/js/script.js"></script>
 </head>
 <body>
@@ -52,9 +26,7 @@
 
 <nav class="lang">
 <div class="time">
-                <div id="datetime" class="mr-3"></div>
-                
-              
+        <div id="datetime" class="mr-3"></div>          
     </div>
         <div class="language">
         <ul>
@@ -71,38 +43,20 @@
         </ul>
         </div>
         <div class="temp"><div id="city" class="mr-3">Lucknow</div>
-                <i id="weather-icon" class="fas fa-sun"></i>
-                <div id="temperature" class="">28°C</div></div>
+            <i id="weather-icon" class="fas fa-sun"></i>
+            <div id="temperature">28°C</div>
+        </div>
     </nav>
 <div class="logo-main">
     
     <div class="logo">
                 <h1>आर्य प्रवाह
-                <h5>हिन्दी दैनिक
-                </h5>
+                <h5>हिन्दी दैनिक</h5>
                 </h1>
     </div>
     <div class="logo-right">
-            <!-- <budiv.tton type="button" class="btn btn-danger">Danger</budiv.tton> -->
-               <button type="button" class="btn btn-danger login"> <a href="/login" class="login-text"><i class="fas fa-user icon"></i>Login</a></button>
+        <button type="button" class="btn btn-danger login"> <a href="/login" class="login-text"><i class="fas fa-user icon"></i>Login</a></button>
     </div>
 </div>
-
-    <div class="navbar">
-          
-        <ul class="navbar-ul">
-            <li><a href="">होम</a></li>
-            <li><a href="#">राष्ट्रीय</a></li>
-            <li><a href="#">राज्य</a></li>
-            <li><a href="#">ओपिनियन</a></li>
-            <li><a href="#">मनोरंजन</a></li>
-            <li><a href="#">खेल</a></li>
-            <li><a href="#">वेब स्टोरीज​​</a></li>
-            <li><a href="#">वीडियो</a></li>
-            <li><a href="#">विश्व</a></li>
-            <li><a href="#">बिजनेस</a></li>
-        </ul>
-
-    </div>
 </body>
 </html>
--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,33 +1,23 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav flex-column">
-            <li class="nav-item"><a class="nav-link" href="/news">News</a></li>
-            <li class="nav-item"><a class="nav-link" href="/city">City</a></li>
-            <li class="nav-item"><a class="nav-link" href="/sports">Sports</a></li>
-            <li class="nav-item"><a class="nav-link" href="/movies">Movies</a></li>
-            <li class="nav-item"><a class="nav-link" href="/lifestyle">Lifestyle</a></li>
-            <li class="nav-item"><a class="nav-link" href="/auto">Auto</a></li>
-            <li class="nav-item"><a class="nav-link" href="/gadgets">Gadgets</a></li>
-            <li class="nav-item"><a class="nav-link" href="/coupons">Coupons</a></li>
-            <li class="nav-item"><a class="nav-link" href="/money">Money</a></li>
+<div class="navbar">  
+        <ul class="navbar-ul">
+            <li><a href="">होम</a></li>
+            <li><a href="#">राष्ट्रीय</a></li>
+            <li><a href="#">राज्य</a></li>
+            <li><a href="#">ओपिनियन</a></li>
+            <li><a href="#">मनोरंजन</a></li>
+            <li><a href="#">खेल</a></li>
+            <li><a href="#">वेब स्टोरीज​​</a></li>
+            <li><a href="#">वीडियो</a></li>
+            <li><a href="#">विश्व</a></li>
+            <li><a href="#">बिजनेस</a></li>
         </ul>
-    </div>
-</nav>
-<div class="container-fluid mt-3">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Search...">
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button"><img src="/path-to-search-icon/search.png" alt="Search"></button>
-                </div>
+
+        <div class="right-side">
+            <button class="download-button">Download E-paper</button>
+            
+            <div class="search-box">
+            <input type="text" class="text" placeholder="Search...">
+            <span class="material-symbols-outlined">search</span>
             </div>
         </div>
-        <div class="col-md-4 text-right">
-            <a href="/e-paper" class="btn btn-primary">Download E-Paper</a>
-        </div>
-    </div>
 </div>
